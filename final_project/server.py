4ffcc6cac3673726fc945b3d145c4eb527ebdd60
PadmaIBM <Padmavathi.Jayaraman@ibm.com>
2022-07-16 01:53:31 +0000

    finalcommit

--- final_project/server.py
+++ final_project/server.py
@@ -5,25 +5,25 @@ import json
 
 app = Flask("Web Translator")
 
-@app.route("/")
-def index():
-    return render_template('index.html')
+
 
 @app.route("/english_to_french")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
     # Write your code here
-    return "Translated text to French"
+    french_text = english_to_french(textToTranslate)
+    return french_text
 
 @app.route("/french_to_english")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
     # Write your code here
-    return "Translated text to English"
+    english_text = french_to_english(textToTranslate)
+    return english_text
 
 @app.route("/")
 def renderIndexPage():
-    # Write the code to render template
+    return render_template('index.html')
 
-#if __name__ == "__main__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
